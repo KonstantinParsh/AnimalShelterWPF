f5452a011e075989458fa7ac2bf6b577f38c2375
KonstantinParsh <167069919+KonstantinParsh@users.noreply.github.com>
2026-04-22 20:28:16 +0400

    Add files via upload

---
+++ Core/UserSession.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Lab4ParshArestGol.Core
+{
+    public static class UserSession
+    {
+        public static Frame MainFrame { get; set; }
+        public static int CurrentUserId { get; set; }
+        public static string CurrentUserFullName { get; set; }
+        public static int CurrentRoleId { get; set; }
+
+        public static bool IsAuthorized()
+        {
+            return CurrentUserId > 0;
+        }
+
+        public static void Logout()
+        {
+            CurrentUserId = 0;
+            CurrentUserFullName = null;
+            CurrentRoleId = 0;
+        }
+    }
+}
